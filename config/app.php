67524f14f0bc92cc205e70ac93f15e56e69d8394
Bosko Stankovic <zekave@yahoo.com>
2018-04-12 14:39:51 +0200

    Changed APP_NAME and VERSION

--- config/app.php
+++ config/app.php
@@ -17,6 +17,18 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Application version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'version' => env('APP_VERSION', '0.1'),
+    /*
+    |--------------------------------------------------------------------------
     | Application Environment
     |--------------------------------------------------------------------------
     |
